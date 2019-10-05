240d7c559af09d3cdb218bf79a6eff4dfb52a090
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2019-10-05 14:57:42 +0200

    erreur dans fichiers SQL et changement du nom des variables

--- script.sql
+++ script.sql
@@ -1,11 +1,11 @@
-CREATE TABLE agences
+CREATE TABLE agence
 (
     id INT PRIMARY KEY NOT NULL auto_increment,
     code INT NOT NULL UNIQUE,
     adresse VARCHAR(255)
 );
 
-CREATE TABLE comptes
+CREATE TABLE compte
 (
     id INT PRIMARY KEY NOT NULL auto_increment,
     solde FLOAT NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE comptes
         REFERENCES agence(code)
 );
 
-CREATE TABLE operations
+CREATE TABLE operation
 (
     id INT PRIMARY KEY NOT NULL auto_increment,
     agence INT NOT NULL,
@@ -32,14 +32,14 @@ CREATE TABLE operations
         REFERENCES compte(id)
 );
 
-INSERT INTO agences (code, adresse)
+INSERT INTO agence (code, adresse)
  VALUES
  (44001, 'Nantes centre'),
  (44002, 'Nantes Rezé'),
  (44003, 'Guerande'),
  (75001, 'Paris');
 
- INSERT INTO comptes (solde, agence, type)
+ INSERT INTO compte (solde, agence, type)
  VALUES
  (0.00, 44001, 1),
  (1000.00, 44001, 3),
@@ -49,13 +49,13 @@ INSERT INTO agences (code, adresse)
  (100.00, 75001, 1),
  (50000.00, 750011, 2);
 
- INSERT INTO operations (agence, compte, type, montant, date_action)
+ INSERT INTO operation (agence, compte, type, montant, date_action)
  VALUES
  (44001, 1, 1, -20.00, CURRENT_TIMESTAMP),
- (75001, 7),
- (44002, 5),
- (44003, 6),
- (75001, 4),
- (75001, 3),
- (75001, 2);
-I
+ (75001, 7,1,-2.00, CURRENT_TIMESTAMP),
+ (44002, 5,2,-3.00, CURRENT_TIMESTAMP),
+ (44003, 6,1,-50.00, CURRENT_TIMESTAMP),
+ (75001, 4,1,-500.00, CURRENT_TIMESTAMP),
+ (75001, 3,2,89.00, CURRENT_TIMESTAMP),
+ (75001, 2,1,00.00, CURRENT_TIMESTAMP);
+
