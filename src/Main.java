27e2085542a926cfd3376147fe13be54cde6451e
Corentin <cortech12@gmail.com>
2019-10-06 18:14:53 +0200

    main modifi compte

--- src/Main.java
+++ src/Main.java
@@ -8,48 +8,68 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     private static IDAO<Integer, Operation> operationDAO = new OperationDAO();
+    private static Scanner sc = new Scanner( System.in );
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
 
         IDAO<Integer, Compte> dao = new CompteDAO();
         Compte compte =  new CompteEpargne(1, 5000, 266);
-        int choice;
-        do{
-            System.out.println("Vous voulez modifier le compte ?");
+        int response;
+        boolean first = true;
+        do {
+            if ( !first ) {
+                System.out.println( "Mauvais choix... merci de recommencer !" );
+            }
+
+            System.out.println("Vous voulez modifier le compte actuel ?");
             System.out.println("1 : Compte Simple");
             System.out.println("2 : Compte Payant");
             System.out.println("3 : Compte Epargne");
             System.out.println("4 : Annuler");
-            Scanner sc = new Scanner(System.in);
-             choice = sc.nextInt();
-        }while(choice != 1|choice != 2 | choice != 3|choice != 4);
-        if(choice == 1){
-            if(compte instanceof CompteSimple){
-                System.out.println("le compte sélectionné est déjà de ce type.");
-            }else{
-                CompteSimple compteSimple = new CompteSimple(compte.getId(), compte.getSolde(),compte.getNumAgence());
-                dao.update(compteSimple);
+            try {
+                response = sc.nextInt();
+            } catch ( InputMismatchException e ) {
+                response = -1;
+            } finally {
+                sc.nextLine();
             }
-        }else  if(choice == 2){
-            if(compte instanceof ComptePayant){
-                System.out.println("le compte sélectionné est déjà de ce type.");
-            }else{
-                ComptePayant comptePayant= new ComptePayant(compte.getId(), compte.getSolde(),compte.getNumAgence());
-                dao.update(comptePayant);
-            }
+            first = false;
+        } while ( response < 1 || response > 4 );
 
-        }else  if(choice == 3){
-            if(compte instanceof CompteEpargne){
-                System.out.println("le compte sélectionné est déjà de ce type.");
-            }else{
-                CompteEpargne compteEpargne= new CompteEpargne(compte.getId(), compte.getSolde(),compte.getNumAgence());
-                dao.update(compteEpargne);
-            }
+        switch ( response ) {
+            case 1:
+                if(compte instanceof CompteSimple){
+                    System.out.println("le compte sélectionné est déjà de ce type.");
+                }else{
+                    System.out.println("passage du compte en compte simple");
+                    CompteSimple compteSimple = new CompteSimple(compte.getId(), compte.getSolde(),compte.getNumAgence());
+                    dao.update(compteSimple);
+                }
+                break;
+            case 2:
+                if(compte instanceof ComptePayant){
+                    System.out.println("le compte sélectionné est déjà de ce type.");
+                }else{
+                    System.out.println("passage du compte en compte payant");
+                    ComptePayant comptePayant= new ComptePayant(compte.getId(), compte.getSolde(),compte.getNumAgence());
+                    dao.update(comptePayant);
+                }
+                break;
+            case 3:
+                if(compte instanceof CompteEpargne){
+                    System.out.println("le compte sélectionné est déjà de ce type.");
+                }else{
+                    System.out.println("passage du compte en compte epargne");
+                    CompteEpargne compteEpargne= new CompteEpargne(compte.getId(), compte.getSolde(),compte.getNumAgence());
+                    dao.update(compteEpargne);
+                }
+                break;
         }
 
         /*
@@ -112,5 +132,5 @@ public class Main {
          */
 
 
-    }
+}
 }
